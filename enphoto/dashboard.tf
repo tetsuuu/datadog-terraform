e444c8695430424dd6fcfab272717887cbcd9640
uluru-kinoshitat <t_kinoshita@uluru.jp>
2019-09-10 11:36:20 +0900

    fix indent && remove default setting

--- enphoto/dashboard.tf
+++ enphoto/dashboard.tf
@@ -1,311 +1,301 @@
 resource "datadog_dashboard" "project_enphoto" {
-    is_read_only = false
-    layout_type  = "ordered"
-    notify_list  = []
-    title        = "project:en-photo"
+  is_read_only = false
+  layout_type  = "ordered"
+  title        = "project:en-photo"
 
-    template_variable {
-        default = "*"
-        name    = "var"
+  template_variable {
+    default = "*"
+    name    = "var"
+  }
+
+  widget {
+
+    timeseries_definition {
+      show_legend = false
+      title       = "Avg of aws.elasticache.freeable_memory over project:enphoto"
+
+      request {
+        display_type = "line"
+        q            = "avg:aws.elasticache.freeable_memory{project:enphoto}"
+      }
     }
+  }
 
-    widget {
-        layout = {}
+  widget {
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "Avg of aws.elasticache.freeable_memory over project:enphoto"
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web disk free"
 
-            request {
-                display_type = "line"
-                q            = "avg:aws.elasticache.freeable_memory{project:enphoto}"
-            }
-        }
+      request {
+        display_type = "line"
+        q            = "avg:system.disk.free{project:enphoto}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web disk free"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "avg:system.disk.free{project:enphoto}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web ELB request count"
+
+      request {
+        display_type = "bars"
+        q            = "max:aws.elb.httpcode_backend_2xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.elb.httpcode_backend_3xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.elb.httpcode_backend_4xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.elb.httpcode_backend_5xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count()"
+      }
+
+      request {
+        display_type = "bars"
+        q            = "max:aws.applicationelb.httpcode_target_2xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.applicationelb.httpcode_target_3xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.applicationelb.httpcode_target_4xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.applicationelb.httpcode_target_5xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count()"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web ELB request count"
+  widget {
 
-            request {
-                display_type = "bars"
-                q            = "max:aws.elb.httpcode_backend_2xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.elb.httpcode_backend_3xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.elb.httpcode_backend_4xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.elb.httpcode_backend_5xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count()"
-            }
-            request {
-                display_type = "bars"
-                q            = "max:aws.applicationelb.httpcode_target_2xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.applicationelb.httpcode_target_3xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.applicationelb.httpcode_target_4xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count(), max:aws.applicationelb.httpcode_target_5xx{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}.as_count()"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web ELB latency time"
+
+      request {
+        display_type = "line"
+        q            = "max:aws.elb.latency{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
+
+      request {
+        display_type = "line"
+        q            = "max:aws.applicationelb.target_response_time.average{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web ELB latency time"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "max:aws.elb.latency{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-            request {
-                display_type = "line"
-                q            = "max:aws.applicationelb.target_response_time.average{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web apache processes"]
+
+      request {
+        display_type = "area"
+        q            = "sum:apache.performance.idle_workers{elasticbeanstalk:environment-name:enphoto-prod-web}, sum:apache.performance.busy_workers{elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web apache processes"
+  widget {
 
-            request {
-                display_type = "area"
-                q            = "sum:apache.performance.idle_workers{elasticbeanstalk:environment-name:enphoto-prod-web}, sum:apache.performance.busy_workers{elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web load avg"
+
+      request {
+        display_type = "line"
+        q            = "sum:system.load.1{elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
+
+      request {
+        display_type = "line"
+        q            = "sum:system.load.5{elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
+
+      request {
+        display_type = "line"
+        q            = "sum:system.load.15{elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web load avg"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "sum:system.load.1{elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-            request {
-                display_type = "line"
-                q            = "sum:system.load.5{elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-            request {
-                display_type = "line"
-                q            = "sum:system.load.15{elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web CPU %"
+
+      request {
+          display_type = "area"
+          q            = "avg:system.cpu.idle{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.system{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.iowait{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.user{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.stolen{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.guest{elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web CPU %"
+  widget {
 
-            request {
-                display_type = "area"
-                q            = "avg:system.cpu.idle{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.system{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.iowait{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.user{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.stolen{elasticbeanstalk:environment-name:enphoto-prod-web}, avg:system.cpu.guest{elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web mem usage"
+
+      request {
+        display_type = "area"
+        q            = "avg:system.mem.total{elasticbeanstalk:environment-name:enphoto-prod-web}-avg:system.mem.usable{elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
+
+      request {
+        display_type = "line"
+        q            = "avg:system.mem.total{elasticbeanstalk:environment-name:enphoto-prod-web}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web mem usage"
+  widget {
 
-            request {
-                display_type = "area"
-                q            = "avg:system.mem.total{elasticbeanstalk:environment-name:enphoto-prod-web}-avg:system.mem.usable{elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-            request {
-                display_type = "line"
-                q            = "avg:system.mem.total{elasticbeanstalk:environment-name:enphoto-prod-web}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto cloudfront 4xx %"
+
+      request {
+        display_type = "bars"
+        q            = "avg:aws.cloudfront.4xx_error_rate{distributionid:e1jd6pajlbwj2q}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto cloudfront 4xx %"
+  widget {
 
-            request {
-                display_type = "bars"
-                q            = "avg:aws.cloudfront.4xx_error_rate{distributionid:e1jd6pajlbwj2q}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto cloudfront 5xx %"
+
+      request {
+        display_type = "line"
+        q            = "avg:aws.cloudfront.5xx_error_rate{distributionid:e1jd6pajlbwj2q}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto cloudfront 5xx %"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "avg:aws.cloudfront.5xx_error_rate{distributionid:e1jd6pajlbwj2q}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-web network in/out"
+
+      request {
+        display_type = "line"
+        q            = "top(avg:aws.ec2.network_in{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}, 10, 'mean', 'desc')"
+      }
+
+      request {
+        display_type = "line"
+        q            = "top(avg:aws.ec2.network_out{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}, 10, 'mean', 'desc')"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-web network in/out"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "top(avg:aws.ec2.network_in{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}, 10, 'mean', 'desc')"
-            }
-            request {
-                display_type = "line"
-                q            = "top(avg:aws.ec2.network_out{project:enphoto,elasticbeanstalk:environment-name:enphoto-prod-web}, 10, 'mean', 'desc')"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod SQS queue count"
+
+      request {
+        display_type = "line"
+        q            = "max:aws.sqs.approximate_number_of_messages_visible{project:enphoto,queuename:enphoto-prod}, max:aws.sqs.approximate_number_of_messages_not_visible{project:enphoto,queuename:enphoto-prod}"
+      }
+
+      request {
+        display_type = "line"
+        q            = "max:aws.sqs.approximate_number_of_messages_visible{project:enphoto,queuename:enphoto-prod-high}, max:aws.sqs.approximate_number_of_messages_not_visible{project:enphoto,queuename:enphoto-prod-high}"
+      }
+
+      request {
+        display_type = "line"
+        q            = "max:aws.sqs.approximate_number_of_messages_visible{project:enphoto,queuename:enphoto-prod-low}, max:aws.sqs.approximate_number_of_messages_not_visible{project:enphoto,queuename:enphoto-prod-low}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod SQS queue count"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "max:aws.sqs.approximate_number_of_messages_visible{project:enphoto,queuename:enphoto-prod}, max:aws.sqs.approximate_number_of_messages_not_visible{project:enphoto,queuename:enphoto-prod}"
-            }
-            request {
-                display_type = "line"
-                q            = "max:aws.sqs.approximate_number_of_messages_visible{project:enphoto,queuename:enphoto-prod-high}, max:aws.sqs.approximate_number_of_messages_not_visible{project:enphoto,queuename:enphoto-prod-high}"
-            }
-            request {
-                display_type = "line"
-                q            = "max:aws.sqs.approximate_number_of_messages_visible{project:enphoto,queuename:enphoto-prod-low}, max:aws.sqs.approximate_number_of_messages_not_visible{project:enphoto,queuename:enphoto-prod-low}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "SES bounce & complaint %"
+
+      marker {
+        display_type = "warning bold"
+        label        = "5%"
+        value        = "y = 5"
+      }
+
+      marker {
+        display_type = "error bold"
+        label        = "10%"
+        value        = "y = 10"
+      }
+
+      request {
+        display_type = "line"
+        q            = "max:aws.ses.reputation_bounce_rate{project:enphoto}, max:aws.ses.reputation_bounce_rate{project:enphoto}*100"
+      }
+
+      request {
+        display_type = "line"
+        q            = "max:aws.ses.reputation_complaint_rate{project:enphoto}, max:aws.ses.reputation_complaint_rate{project:enphoto}*100"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "SES bounce & complaint %"
+  widget {
 
-            marker {
-                display_type = "warning bold"
-                label        = "5%"
-                value        = "y = 5"
-            }
-            marker {
-                display_type = "error bold"
-                label        = "10%"
-                value        = "y = 10"
-            }
+    timeseries_definition {
+      show_legend = false
+      title       = "Avg of aws.rds.free_storage_space over name:enphoto-prod"
 
-            request {
-                display_type = "line"
-                q            = "max:aws.ses.reputation_bounce_rate{project:enphoto}, max:aws.ses.reputation_bounce_rate{project:enphoto}*100"
-            }
-            request {
-                display_type = "line"
-                q            = "max:aws.ses.reputation_complaint_rate{project:enphoto}, max:aws.ses.reputation_complaint_rate{project:enphoto}*100"
-            }
-        }
+      request {
+        display_type = "line"
+        q            = "avg:aws.rds.free_storage_space{name:enphoto-prod}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "Avg of aws.rds.free_storage_space over name:enphoto-prod"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "avg:aws.rds.free_storage_space{name:enphoto-prod}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "Avg of aws.ec2.disk_read_ops over *"
+
+      request {
+        display_type = "line"
+        q            = "avg:aws.ec2.disk_read_ops{*}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "Avg of aws.ec2.disk_read_ops over *"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "avg:aws.ec2.disk_read_ops{*}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "Avg of aws.ec2.cpuutilization over *"
+
+      request {
+        display_type = "line"
+        q            = "avg:aws.ec2.cpuutilization{*}"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "Avg of aws.ec2.cpuutilization over *"
+  widget {
 
-            request {
-                display_type = "line"
-                q            = "avg:aws.ec2.cpuutilization{*}"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "enphoto-prod-lambda-trimming-errors"
+
+      request {
+        display_type = "bars"
+        q            = "avg:aws.lambda.errors{project:enphoto,functionname:enphoto-prod-trimming}.as_count()"
+      }
     }
-    widget {
-        layout = {}
+  }
 
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "enphoto-prod-lambda-trimming-errors"
+  widget {
 
-            request {
-                display_type = "bars"
-                q            = "avg:aws.lambda.errors{project:enphoto,functionname:enphoto-prod-trimming}.as_count()"
-            }
-        }
+    timeseries_definition {
+      show_legend = false
+      title       = "Avg of aws.rds.free_storage_space over *"
+
+      request {
+        display_type = "line"
+        q            = "avg:aws.rds.free_storage_space{*}"
+      }
     }
-    widget {
-        layout = {}
-
-        timeseries_definition {
-            show_legend = false
-            time        = {}
-            title       = "Avg of aws.rds.free_storage_space over *"
-
-            request {
-                display_type = "line"
-                q            = "avg:aws.rds.free_storage_space{*}"
-            }
-        }
-    }
+  }
 }
--- kurapuri/monitor.tf
+++ kurapuri/monitor.tf
@@ -1,95 +1,60 @@
 resource "datadog_monitor" "webserver_freeable_strage" {
-    evaluation_delay    = 0
-    include_tags        = true
-    locked              = false
-    message             = "@slack-alert_kurapuri"
-    name                = "kurapuri-prod-web disk free lowly"
-    new_host_delay      = 300
-    no_data_timeframe   = 0
-    notify_audit        = false
-    notify_no_data      = false
-    query               = "avg(last_5m):avg:system.disk.free{name:kurapuri-prod-web} < 1000000000"
-    renotify_interval   = 0
-    require_full_window = true
-    silenced            = {}
-    tags                = []
-    threshold_windows   = {}
-    thresholds          = {
-        "critical" = "1000000000.0"
-        "warning"  = "4000000000.0"
-    }
-    timeout_h           = 0
-    type                = "metric alert"
+  message             = "@slack-alert_kurapuri"
+  name                = "kurapuri-prod-web disk free lowly"
+  new_host_delay      = 300
+  no_data_timeframe   = 0
+  query               = "avg(last_5m):avg:system.disk.free{name:kurapuri-prod-web} < 1000000000"
+  require_full_window = true
+  thresholds          = {
+    "critical" = "1000000000.0"
+    "warning"  = "4000000000.0"
+  }
+  timeout_h           = 0
+  type                = "metric alert"
 }
 
 resource "datadog_monitor" "rds_freeable_strage" {
-    evaluation_delay    = 0
-    include_tags        = true
-    locked              = false
-    message             = "@slack-alert_kurapuri"
-    name                = "kurapuri rds free storage lowly"
-    new_host_delay      = 300
-    no_data_timeframe   = 0
-    notify_audit        = false
-    notify_no_data      = false
-    query               = "avg(last_5m):avg:aws.rds.free_storage_space{dbname:kurapuri} < 2000000000"
-    renotify_interval   = 0
-    require_full_window = true
-    silenced            = {}
-    tags                = []
-    threshold_windows   = {}
-    thresholds          = {
-        "critical" = "2000000000.0"
-        "warning"  = "3000000000.0"
-    }
-    timeout_h           = 0
-    type                = "metric alert"
+  message             = "@slack-alert_kurapuri"
+  name                = "kurapuri rds free storage lowly"
+  new_host_delay      = 300
+  no_data_timeframe   = 0
+  query               = "avg(last_5m):avg:aws.rds.free_storage_space{dbname:kurapuri} < 2000000000"
+  require_full_window = true
+  thresholds          = {
+    "critical" = "2000000000.0"
+    "warning"  = "3000000000.0"
+  }
+  timeout_h           = 0
+  type                = "metric alert"
 }
 
 resource "datadog_monitor" "elasticache_frreable_memoly" {
-    evaluation_delay    = 0
-    include_tags        = true
-    locked              = false
-    message             = "@slack-alert_kurapuri"
-    name                = "kurapuri elasticahe free mem lowly"
-    new_host_delay      = 300
-    no_data_timeframe   = 0
-    notify_audit        = false
-    notify_no_data      = false
-    query               = "avg(last_5m):avg:aws.elasticache.freeable_memory{name:kurapuri-prod-redis} < 128000000"
-    renotify_interval   = 0
-    require_full_window = true
-    silenced            = {}
-    tags                = []
-    threshold_windows   = {}
-    thresholds          = {
-        "critical" = "128000000.0"
-        "warning"  = "256000000.0"
-    }
-    timeout_h           = 0
-    type                = "metric alert"
+  message             = "@slack-alert_kurapuri"
+  name                = "kurapuri elasticahe free mem lowly"
+  new_host_delay      = 300
+  no_data_timeframe   = 0
+  query               = "avg(last_5m):avg:aws.elasticache.freeable_memory{name:kurapuri-prod-redis} < 128000000"
+  renotify_interval   = 0
+  require_full_window = true
+  thresholds          = {
+    "critical" = "128000000.0"
+    "warning"  = "256000000.0"
+  }
+  timeout_h           = 0
+  type                = "metric alert"
 }
 
 resource "datadog_monitor" "cloudfront_5xx_count" {
-    evaluation_delay    = 0
-    include_tags        = true
-    locked              = false
-    message             = "@slack-alert_kurapuri"
-    name                = "kurapuri CloudFront 5xx rate is highly"
-    new_host_delay      = 300
-    no_data_timeframe   = 0
-    notify_audit        = false
-    notify_no_data      = false
-    query               = "avg(last_5m):avg:aws.cloudfront.5xx_error_rate{distributionid:e2lpz2n9zxv63z} > 0.1"
-    renotify_interval   = 0
-    require_full_window = true
-    silenced            = {}
-    tags                = []
-    threshold_windows   = {}
-    thresholds          = {
-        "critical" = "0.1"
-        "warning"  = "0.05"
-    }
-    timeout_h           = 0
-    type                = "metric alert"
+  message             = "@slack-alert_kurapuri"
+  name                = "kurapuri CloudFront 5xx rate is highly"
+  new_host_delay      = 300
+  no_data_timeframe   = 0
+  query               = "avg(last_5m):avg:aws.cloudfront.5xx_error_rate{distributionid:e2lpz2n9zxv63z} > 0.1"
+  require_full_window = true
+  thresholds          = {
+    "critical" = "0.1"
+    "warning"  = "0.05"
+  }
+  timeout_h           = 0
+  type                = "metric alert"
 }
